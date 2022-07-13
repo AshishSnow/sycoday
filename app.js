b7e262557cfbfaa4dda1c86b346c53d22d31996b
Ashish Kumar Gupta <71519859+AshishSnow@users.noreply.github.com>
2022-07-13 18:37:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,40 +18,35 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 <p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-success btn-sm" type="button"> DEGREE </button></span> ${degree}</p>
 <p ><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-warning btn-sm" type="button">LOCATION</button></span> ${loc}</p>
 <p style="text-align: center;"><!--more Apply Now  ➠ --></p>
-<p></p>
-<p>Are you looking for a ${title} For Freshers and Experienced Candidates? Here is the latest job for you.</p>
-<p></p>
-<h2>How To Apply For ${title}</h2>
-<p></p>
-<p>You can apply to those jobs or internships through the link below. We hope the above information has helped you use the ${title}.</p>
-<p class="text-break"><img loading="lazy" class="aligncenter wp-image-7566 size-full" src="https://sycobrain.com/wp-content/uploads/2022/03/2022cisco-2.png" alt="CISCO Software Engineer Jobs" width="500" height="200" /></p>
+<p> </p>
+<p>Are you looking for a Synchrony off campus Drive 2022 Senior Analyst For Freshers and Experienced Candidates? Here is the latest job for you.</p>
+<p> </p>
+<h2>How To Apply For Synchrony off campus Drive 2022 Senior Analyst</h2>
+<p> </p>
+<p>You can apply to those jobs or internships through the link below. We hope the above information has helped you use the Synchrony off campus Drive 2022  Senior Analyst.</p>
+<p class="text-break"><img class="aligncenter wp-image-7566 size-full" src="https://sycobrain.com/wp-content/uploads/2022/03/2022cisco-2.png" alt="CISCO Software Engineer Jobs" width="500" height="200" /></p>
 <p style="text-align: center;"><strong>Registration Link</strong></p>
 <p style="text-align: center;"><strong>⇓</strong></p>
-
 <p><script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1874315412689682" crossorigin="anonymous"></script> <!-- super syco latest --> <ins class="adsbygoogle" style="display: block;" data-ad-client="ca-pub-1874315412689682" data-ad-slot="8371587063" data-ad-format="auto" data-full-width-responsive="true"></ins> <script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
+(adsbygoogle = window.adsbygoogle || []).push({});
 </script></p>
-<p style="text-align: center;">Apply Now- <u><a href="${links}" target="_blank" rel="noopener">Click Here</a></U></p>
+<p style="text-align: center;">Apply Now- <u><a href="https://www.firstnaukri.com/careers/customised/landingpage/synchrony/index.html" target="_blank" rel="noopener">Click Here</a></u></p>
 <p><script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1874315412689682" crossorigin="anonymous"></script> <!-- super syco latest --> <ins class="adsbygoogle" style="display: block;" data-ad-client="ca-pub-1874315412689682" data-ad-slot="8371587063" data-ad-format="auto" data-full-width-responsive="true"></ins> <script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
+(adsbygoogle = window.adsbygoogle || []).push({});
 </script></p>
-<p></p>
+<p> </p>
 <h2>Interview Tips</h2>
-<p></p>
+<p> </p>
 <p>Be confident, talk plainly and give your 100%. There is seriously no rocket science for clearing job interviews. They are like easy interviews, a conversation between you and the interviewer.</p>
-<p></p>
-
+<p> </p>
 <h3>What are factors that play a crucial role in the Interview:-</h3>
-<p></p>
-<p>Academics Achievements, Extracurricular activities, Projects and Social work or any useful assignment done or currently doing.</p>
-
-
-<p></p>
+<p> </p>
+<p>Academics Achievements, Extracurricular activities, Projects and Social work or any proper assignment done or currently doing.</p>
+<p> </p>
 <h2>Resume Tips</h2>
-<p></p>
-<p>Last but not least – Don’t lie in your resume.Having a page full of crappy specialities or lies is no good. Better to write what you know and past work experience.Never send resume in Docx file. Always send your resume in pdf file format.</p>
-
-<p></p>
+<p> </p>
+<p>Last but not least – Don’t lie in your resume. Having a page full of crappy specialities or lies is no good. Better to write what you know and past work experience. Never send resume in Docx file. Always send your resume in pdf file format.</p>
+<p> </p>
 <table class="table table-striped" style="width: 100%; height: 120px;">
 <thead class="table-dark">
 <tr style="height: 40px;">
@@ -70,12 +65,11 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 </tr>
 </tbody>
 </table>
-
 <table class="table table-striped table-bordered border-dark" style="width: 100%; height: 240px;">
 <thead class="table-success">
 <tr style="height: 40px;">
 <th style="width: 50%; text-align: right; height: 40px;" scope="col">JOBS BY</th>
-<th style="width: 50%; text-align: left; height: 40px;" scope="col">LOCATION </th>
+<th style="width: 50%; text-align: left; height: 40px;" scope="col">LOCATION</th>
 </tr>
 </thead>
 <tbody>
@@ -101,7 +95,6 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 </tr>
 </tbody>
 </table>
-
 <table class="table table-striped table-bordered border-dark" style="width: 100%; height: 120px;">
 <thead class="table-warning">
 <tr>
@@ -133,33 +126,33 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 </tr>
 </tbody>
 </table>
-<p></p>
+<p> </p>
 <h2>About Sycobrain</h2>
-<p></p>
-<p>We are not associated with any company/agency/agent whose job or internship is posted on sycobrain.com. We are just an information provider for job &amp; internship openings. All Company names, logos, and brands are the Intellectual Property of their respective owners. All company, product, and service names used on this website are for identification purposes only. Sycobrain is an informative platform that provides technical and career growth assistance to students pursuing various fields such as B.Tech, Bsc, B.com etc. It provides information on upcoming off-campus hiring drives, available internships, and free course details at no cost.</p>
-
-<p></p><p></p>
+<p> </p>
+<p>We are not associated with any company/agency/agent whose job or internship is posted on sycobrain.com. We are just an information provider for job &amp; internship openings. All Company names, logos, and brands are the Intellectual Property of their respective owners. All company, product, and service names used on this website are for identification purposes only. Sycobrain is an informative platform that provides technical and career growth assistance to students pursuing various fields such as B.Tech, Bsc, B.com etc. It includes information on upcoming off-campus hiring drives, available internships, and free course details at no cost.</p>
+<p> </p>
+<p> </p>
 <h3 style="text-align: center;">Join Our Groups For Latest Updates</h3>
-<p></p>
+<p> </p>
 <div class="container">
 <div class="row row-cols-3 row-cols-lg-6 g-2 g-lg-3">
 <div class="col">
-<div><a class="btn btn-outline-dark" role="button" href="https://www.instagram.com/job2hire/"><img loading="lazy" class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/insta.png" alt="instagram" width="70" height="60" /></a></div>
+<div><a class="btn btn-outline-dark" role="button" href="https://www.instagram.com/job2hire/"><img class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/insta.png" alt="instagram" width="70" height="60" /></a></div>
 </div>
 <div class="col">
-<div><a class="btn btn-outline-dark" role="button" href="https://www.instagram.com/sycoportal/"><img loading="lazy" class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/insta-1-1.png" alt="sycoportal" width="70" height="60" /></a></div>
+<div><a class="btn btn-outline-dark" role="button" href="https://www.instagram.com/sycoportal/"><img class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/insta-1-1.png" alt="sycoportal" width="70" height="60" /></a></div>
 </div>
 <div class="col">
-<div><a class="btn btn-outline-dark" role="button" href="https://www.linkedin.com/company/sycobrain"><img loading="lazy" class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/linkedin.png" alt="linkedin" width="70" height="60" /></a></div>
+<div><a class="btn btn-outline-dark" role="button" href="https://www.linkedin.com/company/sycobrain"><img class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/linkedin.png" alt="linkedin" width="70" height="60" /></a></div>
 </div>
 <div class="col">
-<div><a class="btn btn-outline-dark" role="button" href="https://t.me/jobalertsycobrain"><img loading="lazy" class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/tele.png" alt="telegram" width="70" height="60" /></a></div>
+<div><a class="btn btn-outline-dark" role="button" href="https://t.me/jobalertsycobrain"><img class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/tele.png" alt="telegram" width="70" height="60" /></a></div>
 </div>
 <div class="col">
-<div><a class="btn btn-outline-dark" role="button" href="https://www.facebook.com/sycoportal"><img loading="lazy" class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/fb.png" alt="fb" width="70" height="60" /></a></div>
+<div><a class="btn btn-outline-dark" role="button" href="https://www.facebook.com/sycoportal"><img class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/fb.png" alt="fb" width="70" height="60" /></a></div>
 </div>
 <div class="col">
-<div><a class="btn btn-outline-dark" role="button" href="https://sycobrain.com/contact/"><img loading="lazy" class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/mail-2.png" alt="mail" width="70" height="60" /></a></div>
+<div><a class="btn btn-outline-dark" role="button" href="https://sycobrain.com/contact/"><img class="rounded-circle" src="https://sycobrain.com/wp-content/uploads/2022/04/mail-2.png" alt="mail" width="70" height="60" /></a></div>
 </div>
 </div>
 </div>
