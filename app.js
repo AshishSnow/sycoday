b2b088d62261426b40499625a3e294e9d06844d9
Ashish Kumar Gupta <71519859+AshishSnow@users.noreply.github.com>
2022-07-13 18:31:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 <p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-success btn-sm" type="button"> DEGREE </button></span> ${degree}</p>
 <p ><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-warning btn-sm" type="button">LOCATION</button></span> ${loc}</p>
 <p style="text-align: center;"><!--more Apply Now  ➠ --></p>
+<p></p>
 <p>Are you looking for a ${title} For Freshers and Experienced Candidates? Here is the latest job for you.</p>
-
+<p></p>
 <h2>How To Apply For ${title}</h2>
-
+<p></p>
 <p>You can apply to those jobs or internships through the link below. We hope the above information has helped you use the ${title}.</p>
 <p class="text-break"><img loading="lazy" class="aligncenter wp-image-7566 size-full" src="https://sycobrain.com/wp-content/uploads/2022/03/2022cisco-2.png" alt="CISCO Software Engineer Jobs" width="500" height="200" /></p>
 <p style="text-align: center;"><strong>Registration Link</strong></p>
@@ -34,23 +35,23 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 <p><script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1874315412689682" crossorigin="anonymous"></script> <!-- super syco latest --> <ins class="adsbygoogle" style="display: block;" data-ad-client="ca-pub-1874315412689682" data-ad-slot="8371587063" data-ad-format="auto" data-full-width-responsive="true"></ins> <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script></p>
-
+<p></p>
 <h2>Interview Tips</h2>
-
+<p></p>
 <p>Be confident, talk plainly and give your 100%. There is seriously no rocket science for clearing job interviews. They are like easy interviews, a conversation between you and the interviewer.</p>
+<p></p>
+
 <h3>What are factors that play a crucial role in the Interview:-</h3>
-<p>1- Academics &amp; Achievements</p>
-<p>2- Extracurricular activities</p>
-<p>3- Projects</p>
-<p>4- Social work or any useful assignment done or currently doing.</p>
-<p>TAKE ONE STEP AT A TIME.</p>
+<p></p>
+<p>Academics Achievements, Extracurricular activities, Projects and Social work or any useful assignment done or currently doing.</p>
 
+
+<p></p>
 <h2>Resume Tips</h2>
+<p></p>
+<p>Last but not least – Don’t lie in your resume.Having a page full of crappy specialities or lies is no good. Better to write what you know and past work experience.Never send resume in Docx file. Always send your resume in pdf file format.</p>
 
-<p>Last but not least – Don’t lie in your resume.</p>
-<p>Having a page full of crappy specialities or lies is no good. Better to write what you know and past work experience.</p>
-<p>Never send resume in Docx file. Always send your resume in pdf file format.</p>
-
+<p></p>
 <table class="table table-striped" style="width: 100%; height: 120px;">
 <thead class="table-dark">
 <tr style="height: 40px;">
@@ -132,16 +133,14 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 </tr>
 </tbody>
 </table>
-
+<p></p>
 <h2>About Sycobrain</h2>
+<p></p>
+<p>We are not associated with any company/agency/agent whose job or internship is posted on sycobrain.com. We are just an information provider for job &amp; internship openings. All Company names, logos, and brands are the Intellectual Property of their respective owners. All company, product, and service names used on this website are for identification purposes only. Sycobrain is an informative platform that provides technical and career growth assistance to students pursuing various fields such as B.Tech, Bsc, B.com etc. It provides information on upcoming off-campus hiring drives, available internships, and free course details at no cost.</p>
 
-<p>We are not associated with any company/agency/agent whose job or internship is posted on sycobrain.com. We are just an information provider for job &amp; internship openings.</p>
-<p>All Company names, logos, and brands are the Intellectual Property of their respective owners. All company, product, and service names used on this website are for identification purposes only.<br />Sycobrain is an informative platform that provides technical and career growth assistance to students pursuing various fields such as B.Tech, Bsc, B.com etc.</p>
-<p>It provides information on upcoming off-campus hiring drives, available internships, and free course details at no cost.</p>
-
-
+<p></p><p></p>
 <h3 style="text-align: center;">Join Our Groups For Latest Updates</h3>
-
+<p></p>
 <div class="container">
 <div class="row row-cols-3 row-cols-lg-6 g-2 g-lg-3">
 <div class="col">
