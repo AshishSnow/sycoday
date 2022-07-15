1eb0e0f4a2607ece2495129dd6602658c3b93301
Ashish Kumar Gupta <71519859+AshishSnow@users.noreply.github.com>
2022-07-15 18:23:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+
  const title = document.getElementById('titles').value
  const comp = document.getElementById('company').value
  const role = document.getElementById('role').value
  const degree = document.getElementById('degree').value
  const loc = document.getElementById('loc').value
  const links = document.getElementById('links').value
+ const head1 = document.getElementById('head1').value
+ const para1 = document.getElementById('para1').value
+ 
+ const head2= document.getElementById('head2').value
+ const para2 = document.getElementById('para2').value
+
 
 
  const result = document.querySelector('.result')
@@ -13,124 +20,50 @@ const res =()=>{
 
    
 
-const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-primary btn-sm" type="button">COMPANY</button></span> <strong> ${comp}</p>
-<p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-danger btn-sm" type="button">ROLE </button></span> <strong> ${role}</p>
-<p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-success btn-sm" type="button"> DEGREE </button></span> <strong> ${degree}</p>
-<p ><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-warning btn-sm" type="button">LOCATION</button></span> <strong> ${loc}</p>
-<p style="text-align: center;"><!--more Apply Now  ➠ --></p>
+const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-primary btn-sm" type="button">COMPANY</button></span> <strong><span style="font-size: 14px;">${comp}</span></strong></p>
+<p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-danger btn-sm" type="button">ROLE</button></span><span style="font-size: 14px;">  <strong>${role}</strong></span></p>
+<p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-success btn-sm" type="button"> DEGREE </button></span> <strong><span style="font-size: 14px;"> ${degree}</span></strong></p>
+<p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-warning btn-sm" type="button">LOCATION</button></span> <strong><span style="font-size: 14px;">${loc}</span></strong></p>
+<p style="text-align: center;"><!--more Apply Now ➠ --></p>
 <p> </p>
-<p>Are you looking for a ${title} For Freshers and Experienced Candidates? Here is the latest job for you.</p>
+<p>Are you looking for a Google Off Campus Drive for Freshers and Experienced Candidates? Here is the latest job for you.</p>
+<p> </p>
+<h2>${head1}</h2>
+<p> </p>
+<p>${para1}</p>
+<p> </p>
+<h2>${head2}</h2>
+<p> </p>
+<p>${para2}</p>
+<p> </p>
+<p style="text-align: center;"><strong><span style="background-color: #ff0000; color: #ffffff;"> Important </span></strong></p>
+<p> </p>
+<p> </p>
+<div class="container">
+<div class="row row-cols-2 row-cols-lg-4 g-2 g-lg-3">
+<div><a class="btn btn-outline-dark" role="button" href="https://sycobrain.com/" rel="preload"><img class="img-thumbnail aligncenter" src="https://sycobrain.com/wp-content/uploads/2022/07/home-1.jpg" alt="Home" width="121" height="121" /></a></div>
+<div><a class="btn btn-outline-warning" role="button" href="https://sycobrain.com/category/" rel="preload"><img class="img-thumbnail aligncenter" src="https://sycobrain.com/wp-content/uploads/2022/06/Free-course-3-1.jpg" alt="category" width="121" height="121" /></a></div>
+<div><a class="btn btn-outline-success" role="button" href="https://sycobrain.com/category/latest-jobs/" rel="preload"><img class="img-thumbnail aligncenter" src="https://sycobrain.com/wp-content/uploads/2022/06/Free-course-2.jpg" alt="Latest Jobs" width="121" height="121" /></a></div>
+<div><a class="btn btn-outline-danger" role="button" href="https://sycobrain.com/category/courses/" rel="preload"><img class="img-thumbnail aligncenter" src="https://sycobrain.com/wp-content/uploads/2022/06/Free-course-1.jpg" alt="FREE COURSES" width="121" height="121" /></a></div>
+</div>
+</div>
 <p> </p>
 <h2>How To Apply For ${title}</h2>
 <p> </p>
 <p>You can apply to those jobs or internships through the link below. We hope the above information has helped you use the ${title}</p>
-<p class="text-break"><img class="aligncenter wp-image-7566 size-full" src="https://sycobrain.com/wp-content/uploads/2022/03/2022cisco-2.png" alt="CISCO Software Engineer Jobs" width="500" height="200" /></p>
-<p style="text-align: center;"><strong>Registration Link</strong></p>
-<p style="text-align: center;"><strong>⇓</strong></p>
+<p class="text-break"><img class="aligncenter wp-image-10737 size-full" src="https://sycobrain.com/wp-content/uploads/2022/06/google-2.png" alt="Google Off Campus Drive for Freshers" width="500" height="200" /><span style="color: #000000; background-color: #ffff99;"><strong> Registration Link Of ${title} </strong></span></p>
+<p style="text-align: center;"> </p>
 <p><script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1874315412689682" crossorigin="anonymous"></script> <!-- super syco latest --> <ins class="adsbygoogle" style="display: block;" data-ad-client="ca-pub-1874315412689682" data-ad-slot="8371587063" data-ad-format="auto" data-full-width-responsive="true"></ins> <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script></p>
-<p style="text-align: center;">Apply Now- <u><a href="${links}" target="_blank" rel="noopener">Click Here</a></u></p>
+<p style="text-align: left;"><strong>Apply Link- <u><a href="${links}" target="_blank" rel="noopener">Click Here (${comp})</a></u></strong></p>
 <p><script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1874315412689682" crossorigin="anonymous"></script> <!-- super syco latest --> <ins class="adsbygoogle" style="display: block;" data-ad-client="ca-pub-1874315412689682" data-ad-slot="8371587063" data-ad-format="auto" data-full-width-responsive="true"></ins> <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script></p>
 <p> </p>
-<h2>Interview Tips</h2>
+<p style="text-align: center;"><strong><span style="background-color: #ccffcc;"><span style="color: #333333;"> </span><span style="color: #808080;"><span style="color: #333333;">Latest Jobs, Internships &amp; Courses</span> </span></span></strong></p>
 <p> </p>
-<p>Be confident, talk plainly and give your 100%. There is seriously no rocket science for clearing job interviews. They are like easy interviews, a conversation between you and the interviewer.</p>
-<p> </p>
-<h3>What are factors that play a crucial role in the Interview:-</h3>
-<p> </p>
-<p>Academics Achievements, Extracurricular activities, Projects and Social work or any proper assignment done or currently doing.</p>
-<p> </p>
-<h2>Resume Tips</h2>
-<p> </p>
-<p>Last but not least – Don’t lie in your resume. Having a page full of crappy specialities or lies is no good. Better to write what you know and past work experience. Never send resume in Docx file. Always send your resume in pdf file format.</p>
-<p> </p>
-<table class="table table-striped" style="width: 100%; height: 120px;">
-<thead class="table-dark">
-<tr style="height: 40px;">
-<th style="width: 50%; text-align: right; height: 40px;" scope="col">Don't</th>
-<th style="width: 50%; text-align: left; height: 40px;" scope="col">Miss</th>
-</tr>
-</thead>
-<tbody>
-<tr style="height: 40px;">
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/">HOME</a></td>
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/latest-jobs/">Latest Jobs</a></td>
-</tr>
-<tr style="height: 40px;">
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/courses/">Free Courses</a></td>
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/internship/">Internship</a></td>
-</tr>
-</tbody>
-</table>
-<table class="table table-striped table-bordered border-dark" style="width: 100%; height: 240px;">
-<thead class="table-success">
-<tr style="height: 40px;">
-<th style="width: 50%; text-align: right; height: 40px;" scope="col">JOBS BY</th>
-<th style="width: 50%; text-align: left; height: 40px;" scope="col">LOCATION</th>
-</tr>
-</thead>
-<tbody>
-<tr style="height: 30px;">
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/bangalore-jobs/">Bangalore</a></td>
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/chennai-jobs/">Chennai</a></td>
-</tr>
-<tr style="height: 30px;">
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/delhi-jobs/">Delhi</a></td>
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/hyderabad-jobs/">Hyderabad</a></td>
-</tr>
-<tr style="height: 30px;">
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/kolkata-jobs/">Kolkata</a></td>
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/gurgaon-jobs/">Gurugram</a></td>
-</tr>
-<tr style="height: 30px;">
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/mumbai-jobs/">Mumbai</a></td>
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/noida-jobs/">Noida</a></td>
-</tr>
-<tr style="height: 30px;">
-<td style="width: 50%; text-align: center; height: 40px;"><a href="https://sycobrain.com/category/pune-jobs/">Pune</a></td>
-<td style="width: 50%; text-align: center; height: 40px;"> </td>
-</tr>
-</tbody>
-</table>
-<table class="table table-striped table-bordered border-dark" style="width: 100%; height: 120px;">
-<thead class="table-warning">
-<tr>
-<th style="width: 33%; text-align: right;" scope="col"> </th>
-<th style="width: 33%; text-align: center;" scope="col">JOBS BY CATEGORY</th>
-<th style="width: 33%; text-align: left;" scope="col"> </th>
-</tr>
-</thead>
-<tbody>
-<tr style="height: 30px;">
-<td style="width: 33%; text-align: center;"><a href="https://sycobrain.com/category/ba/">BA</a></td>
-<td style="width: 33%; text-align: center;"><a href="https://sycobrain.com/category/be/">B.E</a></td>
-<td style="width: 33%; text-align: center;"><a href="https://sycobrain.com/category/bba/">BBA</a></td>
-</tr>
-<tr style="height: 30px;">
-<td style="width: 33%; text-align: center;"><a href="https://sycobrain.com/category/bca/">BCA</a></td>
-<td style="width: 33%; text-align: center;"><a href="https://sycobrain.com/category/bcom/">BCom</a></td>
-<td style="width: 33%; text-align: center;"><a href="https://sycobrain.com/category/bsc/">BSc</a></td>
-</tr>
-<tr style="height: 30px;">
-<td style="width: 33%; text-align: center;"><a href="https://sycobrain.com/category/btech/">BTech</a></td>
-<td style="width: 33.1191%; text-align: center;"><a href="https://sycobrain.com/category/govt/">Govt</a></td>
-<td style="width: 33%; text-align: center;"><a href="https://sycobrain.com/category/mba/">MBA</a></td>
-</tr>
-<tr style="height: 30px;">
-<td style="width: 33%; text-align: center;"><a href="https://sycobrain.com/category/mca/">MCA</a></td>
-<td style="width: 33.1191%; text-align: center;"><a href="https://sycobrain.com/category/msc/">MSC</a></td>
-<td style="width: 33%; text-align: center;"><a href="https://sycobrain.com/category/other/">Other</a></td>
-</tr>
-</tbody>
-</table>
-<p> </p>
-<h2>About Sycobrain</h2>
-<p> </p>
-<p>We are not associated with any company/agency/agent whose job or internship is posted on sycobrain.com. We are just an information provider for job &amp; internship openings. All Company names, logos, and brands are the Intellectual Property of their respective owners. All company, product, and service names used on this website are for identification purposes only. Sycobrain is an informative platform that provides technical and career growth assistance to students pursuing various fields such as B.Tech, Bsc, B.com etc. It includes information on upcoming off-campus hiring drives, available internships, and free course details at no cost.</p>
-<p> </p>
+<p>[pt_view id="68efa53dt1"]</p>
 <p> </p>
 <h3 style="text-align: center;">Join Our Groups For Latest Updates</h3>
 <p> </p>
@@ -156,6 +89,23 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 </div>
 </div>
 </div>
+<p> </p>
+<h2>Interview Tips</h2>
+<p> </p>
+<p>Be confident, talk plainly and give your 100%. There is seriously no rocket science for clearing job interviews. They are like easy interviews, a conversation between you and the interviewer.</p>
+<p> </p>
+<h3>What are factors that play a crucial role in the Interview:-</h3>
+<p> </p>
+<p>Academics Achievements, Extracurricular activities, Projects and Social work or any proper assignment done or currently doing.</p>
+<p> </p>
+<h2>Resume Tips</h2>
+<p> </p>
+<p>Last but not least – Don’t lie in your resume. Having a page full of crappy specialities or lies is no good. Better to write what you know and past work experience. Never send resume in Docx file. Always send your resume in pdf file format.</p>
+<p> </p>
+<h2>About Sycobrain</h2>
+<p> </p>
+<p>We are not associated with any company/agency/agent whose job or internship is posted on sycobrain.com. We are just an information provider for job &amp; internship openings. All Company names, logos, and brands are the Intellectual Property of their respective owners. All company, product, and service names used on this website are for identification purposes only. Sycobrain is an informative platform that provides technical and career growth assistance to students pursuing various fields such as B.Tech, Bsc, B.com etc. It includes information on upcoming off-campus hiring drives, available internships, and free course details at no cost.</p>
+<p> </p>
 
 ${title}
 
@@ -169,8 +119,11 @@ LOCATION: ${loc}
 
 APPLY NOW: `
 
+   
 result.innerText = sol;
- 
+  
+
+
 }
 
 res();
@@ -178,4 +131,3 @@ res();
 const refresh =()=>{
 window.location.reload(false)
 }
-
