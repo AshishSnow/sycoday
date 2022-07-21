53546af70ce96307c3f8386de95cc3e81306ed65
Ashish Kumar Gupta <71519859+AshishSnow@users.noreply.github.com>
2022-07-21 13:22:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,23 +25,23 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 <p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-success btn-sm" type="button"> DEGREE </button></span> <strong><span style="font-size: 14px;"> ${degree}</span></strong></p>
 <p><span style="font-family: Righteous; font-size: 12px;"><button class="btn btn-warning btn-sm" type="button">LOCATION</button></span> <strong><span style="font-size: 14px;">${loc}</span></strong></p>
 <p style="text-align: center;"><!--more Apply Now ➠ --></p>
-<p> </p>
+
 <p>Are you looking for a ${title} for Freshers and Experienced Candidates? Here is the latest job for you.</p>
-<p> </p>
+
 <h2>${head1}</h2>
-<p> </p>
+
 <p>${para1}</p>
-<p> </p>
+
 <h2>${head2}</h2>
-<p> </p>
+>
 <p>${para2}</p>
 
-<p> </p>
+
 <h2>How To Apply For ${title}</h2>
-<p> </p>
+
 <p>You can apply to those jobs or internships through the link below. We hope the above information has helped you use the ${title}</p>
 <p class="text-break"><img class="aligncenter wp-image-10737 size-full" src="https://sycobrain.com/wp-content/uploads/2022/06/google-2.png" alt="Google Off Campus Drive for Freshers" width="500" height="200" /><span style="color: #ffffff; background-color: #ff0000;"><strong> Registration Link Of ${title} </strong></span></p>
-<p style="text-align: center;"> </p>
+
 <p><script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1874315412689682" crossorigin="anonymous"></script> <!-- super syco latest --> <ins class="adsbygoogle" style="display: block;" data-ad-client="ca-pub-1874315412689682" data-ad-slot="8371587063" data-ad-format="auto" data-full-width-responsive="true"></ins> <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script></p>
@@ -49,9 +49,9 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 <p><script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1874315412689682" crossorigin="anonymous"></script> <!-- super syco latest --> <ins class="adsbygoogle" style="display: block;" data-ad-client="ca-pub-1874315412689682" data-ad-slot="8371587063" data-ad-format="auto" data-full-width-responsive="true"></ins> <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script></p>
-<p> </p>
+
 <p style="text-align: center;"><strong>More </strong></p>
-<p> </p>
+
 <div class="container">
 <div class="row row-cols-2 row-cols-lg-4 g-2 g-lg-3">
 <div><a class="btn btn-outline-dark" role="button" href="https://sycobrain.com/" rel="preload"><img class="img-thumbnail aligncenter" src="https://sycobrain.com/wp-content/uploads/2022/07/home-1.jpg" alt="Home" width="121" height="121" /></a></div>
@@ -60,13 +60,13 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 <div><a class="btn btn-outline-danger" role="button" href="https://sycobrain.com/category/courses/" rel="preload"><img class="img-thumbnail aligncenter" src="https://sycobrain.com/wp-content/uploads/2022/06/Free-course-1.jpg" alt="FREE COURSES" width="121" height="121" /></a></div>
 </div>
 </div>
-<p> </p>
+
 <p style="text-align: center;"><strong><span style="background-color: #ccffcc;"><span style="color: #333333;"> </span><span style="color: #808080;"><span style="color: #333333;">Latest Jobs, Internships &amp; Courses</span> </span></span></strong></p>
-<p> </p>
+
 <p>[pt_view id="68efa53dt1"]</p>
-<p> </p>
+
 <h3 style="text-align: center;">Join Our Groups For Latest Updates</h3>
-<p> </p>
+
 <div class="container">
 <div class="row row-cols-3 row-cols-lg-6 g-2 g-lg-3">
 <div class="col">
@@ -154,21 +154,21 @@ const sol = `<p><span style="font-family: Righteous; font-size: 12px;"><button c
 </table>
 <p></p>
 <h3>Interview Tips</h2>
-<p> </p>
+
 <p>Be confident, talk plainly and give your 100%. There is seriously no rocket science for clearing job interviews. They are like easy interviews, a conversation between you and the interviewer.</p>
-<p> </p>
-<h4>What are factors that play a crucial role in the Interview:-</h3>
-<p> </p>
+
+<h6>What are factors that play a crucial role in the Interview:-</h6>
+
 <p>Academics Achievements, Extracurricular activities, Projects and Social work or any proper assignment done or currently doing.</p>
-<p> </p>
+
 <h3>Resume Tips</h2>
-<p> </p>
+
 <p>Last but not least – Don’t lie in your resume. Having a page full of crappy specialities or lies is no good. Better to write what you know and past work experience. Never send resume in Docx file. Always send your resume in pdf file format.</p>
 <p> </p>
-<h5>About Sycobrain</h2>
-<p> </p>
+<h3>About Sycobrain</h2>
+
 <p>We are not associated with any company/agency/agent whose job or internship is posted on sycobrain.com. We are just an information provider for job &amp; internship openings. All Company names, logos, and brands are the Intellectual Property of their respective owners. All company, product, and service names used on this website are for identification purposes only. Sycobrain is an informative platform that provides technical and career growth assistance to students pursuing various fields such as B.Tech, Bsc, B.com etc. It includes information on upcoming off-campus hiring drives, available internships, and free course details at no cost.</p>
-<p> </p>
+
 
 ${title}
 
@@ -180,7 +180,21 @@ DEGREE: ${degree}
 
 LOCATION: ${loc}
 
-Apply Now: `
+Apply Now: 
+
+
+${comp}
+
+${role}
+
+${degree}
+
+${loc}
+
+
+
+
+`
 
    
 result.innerText = sol;
